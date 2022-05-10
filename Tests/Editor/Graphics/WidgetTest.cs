bec8a41699975a61e361056425866366d9ae519c
Rafael Moreira Fonseca <ramofojabuka@gmail.com>
2022-05-10 20:18:16 -0300

    tests(widget): add test case for widgets

--- Tests/Editor/Graphics/WidgetTest.cs
+++ Tests/Editor/Graphics/WidgetTest.cs
@@ -87,5 +87,99 @@ namespace Meteoroid.Graphics.Tests
 
             Assert.IsTrue(children.All(c => ReferenceEquals(c.Element.transform.parent, widget.Element.transform)));
         }
+
+        [Test]
+        public void TestWidgetShouldChangeNewChildrenParent()
+        {
+            var widget = new CustomWidget()
+            {
+                Text = "initial text",
+                Children = new IWidget[] { }
+            };
+
+            var children = new IWidget[]
+            {
+                new CustomWidget(),
+                new CustomWidget(),
+                new CustomWidget(),
+            };
+
+            widget.Children = children;
+
+            Assert.IsTrue(children.All(c => ReferenceEquals(c.Element.transform.parent, widget.Element.transform)));
+
+            children = new IWidget[]
+            {
+                new CustomWidget(),
+                new CustomWidget(),
+                new CustomWidget(),
+            };
+
+            widget.Children = children;
+
+            Assert.IsTrue(children.All(c => ReferenceEquals(c.Element.transform.parent, widget.Element.transform)));
+        }
+
+        [Test]
+        public void TestWidgetShouldUnsubscribeFromOldState()
+        {
+            var state = new State<string>("initial text");
+
+            var widget = new CustomWidget() { Text = state };
+
+            widget.Text = "text changed";
+
+            state.Value = "another text 1";
+            state.Value = "another text 2";
+            state.Value = "another text 3";
+            state.Value = "another text 4";
+
+            Assert.That(widget.StateChangedCount, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestWidgetShouldTriggerParentChangedEventOfChildren()
+        {
+            var children = new IWidget[]
+            {
+                new CustomWidget(),
+                new CustomWidget(),
+                new CustomWidget(),
+            };
+
+            Assert.IsTrue(children.OfType<CustomWidget>().All(c => c.ParentChangedCount == 0));
+
+            var widget = new CustomWidget()
+            {
+                Text = "initial text",
+                Children = children
+            };
+
+            Assert.IsTrue(children.OfType<CustomWidget>().All(c => c.ParentChangedCount == 1));
+        }
+
+        [Test]
+        public void TestWidgetShouldNotTriggerParentChangedIfSameParent()
+        {
+            var children = new IWidget[]
+            {
+                new CustomWidget(),
+                new CustomWidget(),
+                new CustomWidget(),
+            };
+
+            var widget = new CustomWidget()
+            {
+                Text = "initial text"
+            };
+
+            widget.Children = new IWidget[] { children[0], children[1], children[2] };
+
+            Assert.IsTrue(children.OfType<CustomWidget>().All(c => c.ParentChangedCount == 1));
+
+            widget.Children = new IWidget[] { children[0], children[1], children[2] };
+
+            Assert.IsTrue(children.OfType<CustomWidget>().All(c => c.ParentChangedCount == 1));
+        }
     }
 }
